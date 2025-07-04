788097cc2ec5ce731d952e9ee808c897dff29297
Rahul Rathore <142798662+rahulrathore579@users.noreply.github.com>
2025-07-04 13:26:44 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -30,7 +30,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'rahulrathore39769@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Mtlbkuchbhi@1'
+app.config['MAIL_PASSWORD'] = 'kecmfzhhlllnfizw'
 mail = Mail(app)
 
 # Init extensions
